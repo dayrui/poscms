4c53a418e1d550cfee603a53979f105b6f11ce55
成都天睿程序设计 <dayrui@gmail.com@bfed36d8-71c6-404c-98cd-4ba22e370f0c>
2017-05-20 02:52:11 +0000

    增加默认数据

---
+++ cache/data/urlrule.cache.php
@@ -0,0 +1,1 @@
+a:1:{i:1;a:4:{s:2:"id";s:1:"1";s:4:"type";s:1:"0";s:4:"name";s:0:"";s:5:"value";a:8:{s:9:"share_tag";s:0:"";s:14:"share_tag_page";s:0:"";s:12:"share_search";s:0:"";s:17:"share_search_page";s:0:"";s:10:"share_list";s:0:"";s:15:"share_list_page";s:0:"";s:10:"share_show";s:0:"";s:15:"share_show_page";s:0:"";}}}
