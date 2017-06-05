6d8aa69f00f0ec943114e3beb68729e52756cc32
成都天睿程序设计 <dayrui@gmail.com@bfed36d8-71c6-404c-98cd-4ba22e370f0c>
2017-06-05 01:27:36 +0000

    fix index

--- install.php
+++ install.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @since	    version 3.2.0
+ * @author	    Lirui <lirui@dayrui.com>
+ * @license     http://www.dayrui.com/license
+ * @copyright   Copyright (c) 2011 - 9999, Dayrui.Com, Inc. QQ群618659751
+ */
+ 
 header('Content-Type: text/html; charset=utf-8');
 if (!function_exists('mcrypt_encrypt')) {
     exit('PHP未开启Mcrypt扩展');
