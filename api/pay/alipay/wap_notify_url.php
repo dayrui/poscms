ad4b71600e482b52f25c8043c48995b42fac2b11
成都天睿程序设计 <dayrui@gmail.com>
2017-07-07 00:59:06 +0000

    init

--- api/pay/alipay/wap_notify_url.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('DR_PAY_ID', 'alipay/wap');
-define('DR_PAY_FILE', 'notify');
-require dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'index.php';
--- api/uc.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('DISCUZ_ROOT', dirname(dirname(__FILE__)).'/api/ucenter/');
-
-include DISCUZ_ROOT.'api/uc.php';
