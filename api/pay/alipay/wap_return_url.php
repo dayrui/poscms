83e51985016d24ec5565637e4409cc9a1c0454e0
成都天睿程序设计 <dayrui@gmail.com>
2017-07-07 01:44:57 +0000

    no commit message

---
+++ api/pay/alipay/wap_return_url.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DR_PAY_ID', 'alipay/wap');
+define('DR_PAY_FILE', 'return');
+define('IS_MEMBER', TRUE);
+
+require dirname(dirname(dirname(dirname(__FILE__)))).DIRECTORY_SEPARATOR.'index.php';
---
+++ config/domain.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Dayrui Website Management System
+ * 
+ * @since			version 2.7.4
+ * @author			Dayrui <dayrui@gmail.com>
+ * @license     	http://www.dayrui.com/license
+ * @copyright		Copyright (c) 2011 - 9999, Dayrui.Com, Inc.
+ */
+
+/**
+ * 站点域名文件
+ */
+
+return array(
+
+
+
+);
